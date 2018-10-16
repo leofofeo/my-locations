d88611fa5b01382337a0b06e59cbd1ceee94ba7d
Leo <leo.a.rubiano@gmail.com>
2018-10-16 18:07:11 -0500

    more stuff

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Leonardo Rubiano on 16.10.18.
+//  Copyright © 2018 leofofeo. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
--- MyLocations/AppDelegate.swift
+++ MyLocations/AppDelegate.swift
@@ -7,11 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: {
+            storeDescription, error in
+            if let error = error {
+                fatalError("Coud load data store: \(error)")
+            }
+        })
+        return container
+    }()
+    lazy var managedObjectContext: NSManagedObjectContext = self.persistentContainer.viewContext
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
